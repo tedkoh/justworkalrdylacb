626ece53a093bd8df92fca93013d4aa636d7e0af
Why-Tee <32762579+Why-Tee@users.noreply.github.com>
2020-05-17 05:29:26 +0800

    Update and rename getcaccbyid.py to GetCAccByID.py

---
+++ yi tian look here/GetCAccByID.py
@@ -0,0 +1,11 @@
+def GetCAccByID(accountId):
+    import requests
+
+    url = "https://razerhackathon.sandbox.mambu.com/api/savings/{}/"
+    auth = ('Team112','pass19694D02B3')
+    payload  = {}
+    headers = {'': ''}
+
+    response = requests.request("GET", url.format(accountId, headers=headers, data = payload, auth=auth)
+
+    return response.json()
